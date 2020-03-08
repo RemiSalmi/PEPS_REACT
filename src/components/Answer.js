4edaa45ab1f2002f94637d4d98b75a34aa5833eb
Rémi <remi.salmi@gmail.com>
2020-03-08 17:11:23 +0100

    rework answer component to fit with th specif

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -7,18 +7,17 @@ class Answer extends React.Component{
                 <div class="card">
                     <div class="card-body">
                         <h5 class="card-category card-category-social">
-                        <i class="material-icons">place</i> {this.props.location}, {this.props.dateCreation}
                         </h5>
                         <h4 class="card-title">
-                            <a href="#pablo">&quot;{this.props.answer}&quot;</a>
+                            <p>&quot;{this.props.answer}&quot;</p>
                         </h4>
 
                         <div class="card-stats">
                             <div class="author">
-                                <a href="#pablo">
+                                <div>
                                     <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&w=334&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" alt="..." class="avatar img-raised"/>
                                     <span>{this.props.creator}</span>
-                                </a>
+                                </div>
                             </div>
                             <div class="stats ml-auto" style={{'color': '#a65fb3'}}>
                             <i class="fas fa-bullhorn icon-pad"></i> {this.props.nbLike}
