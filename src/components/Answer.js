58ed37a63db7e60183141cb518867d39d7ec0443
Rémi <remi.salmi@gmail.com>
2020-03-10 12:32:11 +0100

    change class in className

--- src/components/Answer.js
+++ src/components/Answer.js
@@ -4,23 +4,23 @@ class Answer extends React.Component{
 
     render(){
         return(
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-category card-category-social">
+                <div className="card">
+                    <div className="card-body">
+                        <h5 className="card-category card-category-social">
                         </h5>
-                        <h4 class="card-title">
+                        <h4 className="card-title">
                             <p>&quot;{this.props.answer}&quot;</p>
                         </h4>
 
-                        <div class="card-stats">
-                            <div class="author">
+                        <div className="card-stats">
+                            <div className="author">
                                 <div>
-                                    <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&w=334&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" alt="..." class="avatar img-raised"/>
+                                    <img src="https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?auto=format&fit=crop&w=334&q=80&ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" alt="..." className="avatar img-raised"/>
                                     <span>{this.props.creator}</span>
                                 </div>
                             </div>
-                            <div class="stats ml-auto" style={{'color': '#a65fb3'}}>
-                            <i class="fas fa-bullhorn icon-pad"></i> {this.props.nbLike}
+                            <div className="stats ml-auto" style={{'color': '#a65fb3'}}>
+                            <i className="fas fa-bullhorn icon-pad"></i> {this.props.nbLike}
                             </div>
                         </div>
                     </div>
