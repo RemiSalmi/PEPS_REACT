5540f0994fa587030e421e54b8d862b451b77616
Rémi <remi.salmi@gmail.com>
2020-03-11 23:44:18 +0100

    use new list system

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,26 +1,16 @@
 import React from 'react';
+
 import Landing from './Landing'
-import List from './List'
 import Footer from './Footer'
-import { connect } from 'react-redux' 
-import { getAllRemarks } from '../actions/remarkAction'
-import { getAllUsers } from '../actions/userAction'
-import { getAllAnswers } from '../actions/answerAction';
- 
+import ListRemark from './ListRemarks'
+
 class Home extends React.Component{
-    componentDidMount(){
-        this.props.getAllAnswers()
-        this.props.getAllUsers()
-        this.props.getAllRemarks()
-        
-        
-    }
 
     render(){
         return (
             <div>
                 <Landing/>
-                <List title="Remarks" type="remark" remarks={this.props.remarks}/>
+                <ListRemark title={"Remarks"}/>
                 <Footer/>
             </div>
          );
@@ -28,19 +18,4 @@ class Home extends React.Component{
     
 }
 
-const mapStateToProps = (state) => {
-    return {
-        remarks : state.remark.remarks,
-        users : state.user.users, 
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        getAllRemarks: () => dispatch(getAllRemarks()),
-        getAllUsers: () => dispatch(getAllUsers()),
-        getAllAnswers: () => dispatch(getAllAnswers())
-    }
-}
-
-export default connect(mapStateToProps,mapDispatchToProps)(Home);
+export default (Home);
