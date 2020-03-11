74fba840c229fb1826b90ce6f5b1b38005e13a11
Rémi Salmi <remi.salmi@gmail.com>
2020-03-11 18:44:48 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render(){
+        return (
+            <footer>
+                <p className="love">Made with <span role="img" aria-label="heart">❤️</span> by <strong>Paola Andreu, Rémi Salmi and Guillaume Tessier</strong></p>
+            </footer>
+        );
+    }
+}
+
+export default Footer
