d6b1fc1656d245ce3217216be9fbd9993ca83849
Rémi Salmi <remi.salmi@gmail.com>
2020-03-11 18:51:00 +0100

    add auth store

---
+++ src/reducers/authReducer.js
@@ -0,0 +1,9 @@
+const initState = {
+    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZFVzZXIiOjI5LCJyb2xlIjoidXNlciIsImlhdCI6MTU4Mjg5ODAzN30.GmstVYka-gZgp_V1gvpkNSI0EmVMXC3txiUeI6VA8XY"
+}
+
+const authReducer = (state = initState, action) =>{
+    return state
+}
+
+export default authReducer
