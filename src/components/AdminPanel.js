c47b1eb0def78c22579ce8a6c51e5e9d950c485a
GAzerty <guilherme.t14@gmail.com>
2020-03-17 11:42:54 +0100

    List of remarks, answers, users available. Button update and delete in position

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux' 
 import { fetchRemarks } from '../actions/remarkAction';
+import { fetchAnswers } from '../actions/answerAction';
+import { fetchUsers } from '../actions/userAction';
 
-import Remark from './Remark'
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+import UpdateIcon from '@material-ui/icons/Update';
+
+import Divider from '@material-ui/core/Divider';
 import Grid from '@material-ui/core/Grid'
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -17,6 +27,8 @@ class AdminPanel extends React.Component{
 
     componentDidMount(){
         this.props.dispatch(fetchRemarks());
+        this.props.dispatch(fetchAnswers());
+        this.props.dispatch(fetchUsers());
     }
 
     constructor(props){
@@ -46,28 +58,117 @@ class AdminPanel extends React.Component{
 
 
     render(){
-        var {remarks} = this.props;
-        console.log(remarks)
+        var {remarks, answers, users} = this.props;
         var adminContent, adminTitle;
+
         if (this.state.handleRemarks){
             adminTitle = <div>Administer all remarks</div>
             adminContent = (
-                <div className={"container"}>
-                      <ul>
+                    <List>
                         {remarks.allIds.length ? (
                             remarks.allIds.map(remarkId => {
-                                return <li key={remarkId}><Remark remark={remarks.byId[remarkId]}></Remark></li>
+                                return (
+                                    
+                                    <div>
+                                    <ListItem key={remarkId}>
+                                        <ListItemText>
+                                        {remarks.byId[remarkId].remark}
+                                        </ListItemText>
+                                        <ListItemSecondaryAction>
+                                            <IconButton aria-label="update">
+                                                <UpdateIcon color="primary"/>
+                                            </IconButton>
+
+                                            <IconButton edge="end" aria-label="delete">
+                                                <DeleteIcon color="secondary"/>
+                                            </IconButton>
+                                        </ListItemSecondaryAction>
+                                    </ListItem>
+                                    <Divider/>
+                                    </div>
+                                )
                             })
-                        ) : (
-                            <p>There is no remarks</p>
-                        )}    
-                    </ul>
-                </div> 
+
+
+                        ) :(
+                            <ListItem>
+                                <ListItemText>No remarks to handle</ListItemText>
+                            </ListItem>
+                            
+                        )}
+                        
+                    </List>
             )
         }else if (this.state.handleAnswers){
             adminTitle = <div>Administer all answers</div>
+            adminContent = (
+                <List>
+                    {answers.allIds.length ? (
+                        answers.allIds.map(answerId => {
+                            return (
+                                
+                                <div>
+                                <ListItem key={answerId}>
+                                    <ListItemText>
+                                    {answers.byId[answerId].answer}
+                                    </ListItemText>
+                                    <ListItemSecondaryAction>
+                                        <IconButton aria-label="update">
+                                            <UpdateIcon color="primary"/>
+                                        </IconButton>
+
+                                        <IconButton edge="end" aria-label="delete">
+                                            <DeleteIcon color="secondary"/>
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                <Divider/>
+                                </div>
+                            )
+                        })
+                    ) :(
+                        <ListItem>
+                            <ListItemText>No answers to handle</ListItemText>
+                        </ListItem>
+                        
+                    )}
+                </List>
+            )
         }else if (this.state.handleUsers){
             adminTitle = <div>Administer all users</div>
+            adminContent = (
+                <List>
+                    {users.allIds.length ? (
+                        users.allIds.map(userId => {
+                            return (
+                                
+                                <div>
+                                <ListItem key={userId}>
+                                    <ListItemText>
+                                    {users.byId[userId].pseudo}
+                                    </ListItemText>
+                                    
+                                    <ListItemSecondaryAction>
+                                        <IconButton aria-label="update">
+                                            <UpdateIcon color="primary"/>
+                                        </IconButton>
+
+                                        <IconButton edge="end" aria-label="delete">
+                                            <DeleteIcon color="secondary"/>
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                <Divider/>
+                                </div>
+                            )
+                        })
+                    ) :(
+                        <ListItem>
+                            <ListItemText>No users to handle</ListItemText>
+                        </ListItem>
+                    )}
+                </List>
+            )
         }else{
             adminTitle = <div> Click to administer </div>
         }
@@ -113,6 +214,8 @@ class AdminPanel extends React.Component{
 }
 const mapStateToProps = state => ({
     remarks: state.remarks,
+    answers: state.answers,
+    users: state.users,
 });
 
 export default connect(mapStateToProps)(AdminPanel);
