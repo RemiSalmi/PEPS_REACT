e31cddc0df911890a75fb7ce1dadf6489bdab8fe
GAzerty <guilherme.t14@gmail.com>
2020-03-20 11:38:58 +0100

    redesign of remarkAdmin, crud for admin

--- src/components/AdminPanel.js
+++ src/components/AdminPanel.js
@@ -9,6 +9,11 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent'; 
+
 import Grid from '@material-ui/core/Grid';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -39,10 +44,8 @@ class AdminPanel extends React.Component{
             handleAnswers: false,
             handleUsers: false,
             handleCategories: false,
+            openDialog: false,
         }
-        this.toggleRemarks = this.toggleRemarks.bind(this)
-        this.toggleAnswers = this.toggleAnswers.bind(this)
-        this.toggleUsers = this.toggleUsers.bind(this)
     }
 
     toggleRemarks = () => {
@@ -61,31 +64,51 @@ class AdminPanel extends React.Component{
         this.setState({handleRemarks: false, handleAnswers:false, handleUsers:false, handleCategories:true})
     }
 
+    handleOpenDialog = () => {
+        this.setState({openDialog:true})
+    }
+
+    handleClose = () => {
+        this.setState({openDialog:false})
+    }
+
 
     render(){
         var {remarks, answers, users, categories} = this.props;
-        var adminContent, adminTitle, createButton;
+        var adminContent, adminTitle, createButton, dialogContent;
 
         if (this.state.handleRemarks){
             adminTitle = <div>Administer remarks</div>
             adminContent = (
-                    <List>
-                        {remarks.allIds.length ? (
-                            remarks.allIds.map(remarkId => {
-                                return (
-                                    <RemarkAdmin remark={remarks.byId[remarkId]}></RemarkAdmin>
-                                )
-                            })
-                        ) :(
-                            <ListItem>
-                                <ListItemText>No remarks to handle</ListItemText>
-                            </ListItem>
-                        )}
-                    </List>
+                    <RemarkAdmin/>
             )
             
-            createButton = <Button variant="contained" color="default" endIcon={<AddIcon/>}>New Remark</Button>
-
+            
+            dialogContent = (
+                <form>
+                    <div className="form-group">
+                        <label htmlFor="Location">Location</label>
+                        <input type="text" className="form-control" id="location" placeholder="Paris" onChange={this.handleChangeLocation}/>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Category">Category</label>
+                        <select className="form-control selectpicker" data-style="btn btn-link" id="Category" onChange={this.handleChangeCategory}>
+                            {
+                            categories.allIds.map(idCategory => {
+                                if(categories.byId[idCategory].type === "remark"){
+                                    return <option key={idCategory} value={idCategory}>{categories.byId[idCategory].lib}</option>
+                                }else{
+                                    return null
+                                }
+                            })}
+                        </select>
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="Remark">Remark</label>
+                        <textarea className="form-control" id="Remark" rows="3" onChange={this.handleChangeRemark}></textarea>
+                    </div>
+                </form> 
+            )
 
 
         }else if (this.state.handleAnswers){
@@ -208,6 +231,23 @@ class AdminPanel extends React.Component{
                     {adminContent}
                 </Grid>
             </Grid>
+
+            <Dialog open={this.state.openDialog} onClose={this.handleClose} aria-labelledby="form-dialog-title" fullWidth={true} maxWidth={'xl'}>
+                    <DialogTitle id="form-dialog-title">Creation</DialogTitle>
+                
+                    <DialogContent>
+                        {dialogContent}
+                    </DialogContent>
+
+                    <DialogActions>
+                        <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
+                            Cancel
+                        </Button>
+                        <Button variant="outlined" color="secondary" autoFocus>
+                            Create
+                        </Button>
+                    </DialogActions>
+            </Dialog>
 
             
             </section>
--- src/components/RemarkAdmin.js
+++ src/components/RemarkAdmin.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { connect } from 'react-redux'
 
 import { fetchUsers } from '../actions/userAction';
-import { deleteRemark } from '../actions/remarkAction';
+import { deleteRemark, addRemarks, updateRemark } from '../actions/remarkAction';
 
 import Dialog from '@material-ui/core/Dialog';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent'; 
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -16,6 +17,7 @@ import IconButton from '@material-ui/core/IconButton';
 import DeleteIcon from '@material-ui/icons/Delete';
 import UpdateIcon from '@material-ui/icons/Update';
 
+import AddIcon from '@material-ui/icons/Add'
 import Divider from '@material-ui/core/Divider';
 var jwt = require('jsonwebtoken');
 
@@ -31,11 +33,22 @@ class RemarkAdmin extends React.Component{
             openDialog: false,
             dialogAction: "",
             remarkId: "",
+            remark:"",
+            idCategory: 1,
+            location:"",
         }
     }
+
+    handleCreate = () => {
+        let remark = {"remark":this.state.remark, "idCategory" : this.state.idCategory, "location" : this.state.location, "token": sessionStorage.getItem('token')}
+        this.props.dispatch(addRemarks(remark));
+        this.handleClose()
+    };
     
     handleUpdate = () => {
-
+        let remark = {"remark":this.state.remark, "idCategory" : this.state.idCategory, "location" : this.state.location}
+        this.props.dispatch(updateRemark(remark,sessionStorage.getItem('token')))
+        this.handleClose()
     };
 
     handleDelete = () =>{
@@ -51,36 +64,40 @@ class RemarkAdmin extends React.Component{
         this.setState({openDialog: true, dialogAction: "update" , remarkId: event.currentTarget.value})
     };
 
+    handleConfirmCreate = () => {
+        this.setState({openDialog:true, dialogAction: "create"})
+    }
+
     handleClose = () => {
         this.setState({ openDialog: false })
     };
 
+    handleChangeLocation = (e) => {
+        this.setState({
+            location: e.target.value
+        })
+    }
+
+    handleChangeCategory = (e) => {
+        this.setState({
+            category: e.target.value
+        })
+    }
+
+    handleChangeRemark = (e) => {
+        this.setState({
+            remark: e.target.value
+        })
+    }
+
     render(){
 
-        const { remark, users } = this.props;
-        var dialogContent, dialogActionsUpdate, dialogActionsDelete;
+        const { remarks, categories } = this.props;
+        var dialogContent, dialogActions;
 
-        dialogActionsDelete = (
-            <DialogActions>
-                <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
-                    Cancel
-                </Button>
-                <Button variant="outlined" color="secondary" autoFocus onClick={this.handleDelete}>
-                    Delete
-                </Button>
-            </DialogActions>
-        )
 
-        dialogActionsUpdate = (
-            <DialogActions>
-                <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
-                    Cancel
-                </Button>
-                <Button variant="outlined" color="primary" autoFocus >
-                    Update
-                </Button>
-            </DialogActions>
-        )
+
+
 
         if (this.state.dialogAction === "delete"){
             dialogContent = (
@@ -88,42 +105,134 @@ class RemarkAdmin extends React.Component{
                     <div>Are you sure to delete this remark ??</div>
                 </DialogContent>
             )
+
+            dialogActions = (
+                <DialogActions>
+                    <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="outlined" color="secondary" autoFocus onClick={this.handleDelete}>
+                        Delete
+                    </Button>
+                </DialogActions>
+            )
         }else if(this.state.dialogAction === "update"){
             dialogContent = (
                 <DialogContent>
-                    <div>Update the remark</div>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="Location">Location</label>
+                            <input type="text" className="form-control" value={remarks.byId[this.state.remarkId].location} id="location"  onChange={this.handleChangeLocation}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="Category">Category</label>
+                            <select className="form-control selectpicker" data-style="btn btn-link" id="Category" onChange={this.handleChangeCategory}>
+                                <option key={remarks.byId[this.state.remarkId].idCategory} value={remarks.byId[this.state.remarkId].idCategory}>{categories.byId[remarks.byId[this.state.remarkId].idCategory].lib}</option>
+                                {
+                                categories.allIds.map(idCategory => {
+                                    if(categories.byId[idCategory].type === "remark"){
+                                        return <option key={idCategory} value={idCategory}>{categories.byId[idCategory].lib}</option>
+                                    }else{
+                                        return null
+                                    }
+                                })}
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="Remark">Remark</label>
+                            <textarea className="form-control" id="Remark" rows="3" value={remarks.byId[this.state.remarkId].remark} onChange={this.handleChangeRemark}></textarea>
+                        </div>
+                    </form> 
                 </DialogContent>
+            )
+
+            dialogActions = (
+                <DialogActions>
+                    <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="outlined" color="primary" autoFocus onClick={this.handleUpdate}>
+                        Update
+                    </Button>
+                </DialogActions>
+            )
+        }else if(this.state.dialogAction === "create"){
+            dialogContent = (
+                <DialogContent>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="Location">Location</label>
+                            <input type="text" className="form-control" id="location" placeholder="Paris" onChange={this.handleChangeLocation}/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="Category">Category</label>
+                            <select className="form-control selectpicker" data-style="btn btn-link" id="Category" onChange={this.handleChangeCategory}>
+                                {
+                                categories.allIds.map(categoryId => {
+                                    if(categories.byId[categoryId].type === "remark"){
+                                        return <option key={categoryId} value={categoryId}>{categories.byId[categoryId].lib}</option>
+                                    }else{
+                                        return null
+                                    }
+                                })}
+                            </select>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="Remark">Remark</label>
+                            <textarea className="form-control" id="Remark" rows="3" onChange={this.handleChangeRemark}></textarea>
+                        </div>
+                    </form>
+                </DialogContent> 
+            )
+
+            dialogActions = (
+                <DialogActions>
+                    <Button variant="outlined" color="default" autoFocus onClick={this.handleClose}>
+                        Cancel
+                    </Button>
+                    <Button variant="outlined" color="primary" autoFocus onClick={this.handleCreate}>
+                        Create
+                    </Button>
+                </DialogActions>
             )
         }
 
         return(
             <div>
-                <ListItem >
-                    <ListItemText key={remark.idRemark}>
-                    {remark.remark}
-                    </ListItemText>
-                    <ListItemSecondaryAction>
-                        <IconButton aria-label="update" value={remark.idRemark} onClick={this.handleConfirmUpdate}>
-                            <UpdateIcon color="primary" />
-                        </IconButton>
+                <Button variant="contained" color="default" endIcon={<AddIcon/>} onClick={this.handleConfirmCreate}>New Remark</Button>
+                <List>
+                    {remarks.allIds.length ? (
+                        remarks.allIds.map(remarkId => {
+                            return (
+                                <div>
+                                <ListItem >
+                                    <ListItemText key={remarkId}>
+                                    {remarks.byId[remarkId].remark}
+                                    </ListItemText>
+                                    <ListItemSecondaryAction>
+                                        <IconButton aria-label="update" value={remarkId} onClick={this.handleConfirmUpdate}>
+                                            <UpdateIcon color="primary" />
+                                        </IconButton>
 
-                        <IconButton edge="end" aria-label="delete" value={remark.idRemark} onClick={this.handleConfirmDelete}>
-                            <DeleteIcon color="secondary"/>
-                        </IconButton>
-                    </ListItemSecondaryAction>
-                </ListItem>
-                <Divider/>
+                                        <IconButton edge="end" aria-label="delete" value={remarkId} onClick={this.handleConfirmDelete}>
+                                            <DeleteIcon color="secondary"/>
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+                                </ListItem>
+                                <Divider/>
+                                </div>
+                            )
+                        })
+                    ) :(
+                        <ListItem>
+                            <ListItemText>No remarks to handle</ListItemText>
+                        </ListItem>
+                    )}
+                </List>
 
                 <Dialog open={this.state.openDialog} onClose={this.handleClose} aria-labelledby="form-dialog-title"  maxWidth={'xl'}>
-                    <DialogTitle id="form-dialog-title">{this.state.dialogAction==="delete" ? ( <p>Verification</p> ):(<p>Update</p>)}</DialogTitle>
-                
                     {dialogContent}
-
-                    {this.state.dialogAction==="delete" ? (
-                        dialogActionsDelete
-                    ): (
-                        dialogActionsUpdate
-                    )}
+                    {dialogActions}
                 </Dialog>
 
             </div>
@@ -134,6 +243,7 @@ class RemarkAdmin extends React.Component{
 const mapStateToProps = state => ({
     users: state.users,
     remarks : state.remarks,
+    categories: state.categories,
 });
 
 export default connect(mapStateToProps)(RemarkAdmin);
